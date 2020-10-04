732fc7b5bfc31b191a2285ae7f8de22f55abd80b
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-10-04 19:16:13 +0200

    Implementado el metodo para poder ejecutar la aplicacion correctamente en Linux

--- Ejercicio_1/src/unica/NavegadorWeb.java
+++ Ejercicio_1/src/unica/NavegadorWeb.java
@@ -9,6 +9,45 @@ import java.util.concurrent.TimeUnit;
 
 public class NavegadorWeb {
     public static void main(String[] args) {
+        String OS = System.getProperty("os.name");
+        if (OS.equals("Windows 10"))
+            navegadorWebWindows();
+        else if (OS.equals("Linux"))
+            navegadorWebLinux();
+        else
+            System.out.println("Esta aplicacion no esta disponible para este sistema operativo");
+    }
+
+    private static void navegadorWebLinux() {
+        String sRutaProceso = "google-chrome";
+
+        ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+        exec.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                // Creacion de proceso
+                String inputTeclado = ValidaLibrary.leer("Introduce la url: ");
+                ProcessBuilder pb = new ProcessBuilder(sRutaProceso, inputTeclado);
+
+                try {
+                    // Lanzamos el proceso
+                    Process pruses = pb.start();
+
+                    // Obtenemos estado de ejecucion
+                    int iRetorno = pruses.waitFor();
+
+                    System.out.println("La ejecucion del proceso " + sRutaProceso + " devuelve " + iRetorno);
+                }catch (IOException e) {
+                    System.out.println("Error IO: " + e.getMessage());
+                    System.exit(-1);
+                }catch (InterruptedException e) {
+                    System.out.println("Error IE: " + e.getMessage());
+                }
+            }
+        }, 0, 10, TimeUnit.SECONDS);
+    }
+
+    private static void navegadorWebWindows() {
         String sRutaProceso = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
 
         ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
@@ -35,6 +74,5 @@ public class NavegadorWeb {
                 }
             }
         }, 0, 10, TimeUnit.SECONDS);
-
     }
 }
