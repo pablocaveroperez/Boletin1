ab0e82572205c38fb7e67e162ced2164299a635e
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-10-04 19:34:16 +0200

    Arreglado el tiempo de espera entre peticiones

--- Ejercicio_1/src/unica/NavegadorWeb.java
+++ Ejercicio_1/src/unica/NavegadorWeb.java
@@ -21,58 +21,55 @@ public class NavegadorWeb {
     private static void navegadorWebLinux() {
         String sRutaProceso = "google-chrome";
 
-        ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
-        exec.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                // Creacion de proceso
-                String inputTeclado = ValidaLibrary.leer("Introduce la url: ");
-                ProcessBuilder pb = new ProcessBuilder(sRutaProceso, inputTeclado);
 
-                try {
-                    // Lanzamos el proceso
-                    Process pruses = pb.start();
+        int iRetorno = 0;
+        do {
+            String inputTeclado = ValidaLibrary.leer("Introduce la url: ");
+            ProcessBuilder pb = new ProcessBuilder(sRutaProceso, inputTeclado);
+            try {
+                // Lanzamos el proceso
+                Process pruses = pb.start();
 
-                    // Obtenemos estado de ejecucion
-                    int iRetorno = pruses.waitFor();
+                // Obtenemos estado de ejecucion
+                iRetorno = pruses.waitFor();
 
-                    System.out.println("La ejecucion del proceso " + sRutaProceso + " devuelve " + iRetorno);
-                }catch (IOException e) {
-                    System.out.println("Error IO: " + e.getMessage());
-                    System.exit(-1);
-                }catch (InterruptedException e) {
-                    System.out.println("Error IE: " + e.getMessage());
-                }
+                System.out.println("La ejecucion del proceso " + sRutaProceso + " devuelve " + iRetorno);
+                Thread.sleep(10000);
+            }catch (IOException e) {
+                System.out.println("Error IO: " + e.getMessage());
+                System.exit(-1);
+            }catch (InterruptedException e) {
+                System.out.println("Error IE: " + e.getMessage());
             }
-        }, 0, 10, TimeUnit.SECONDS);
+
+        }while (iRetorno == 0);
     }
 
     private static void navegadorWebWindows() {
         String sRutaProceso = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
 
-        ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
-        exec.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                // Creacion de proceso
-                String inputTeclado = ValidaLibrary.leer("Introduce la url: ");
-                ProcessBuilder pb = new ProcessBuilder(sRutaProceso, inputTeclado);
 
-                try {
-                    // Lanzamos el proceso
-                    Process pruses = pb.start();
+        int iRetorno = 0;
+        do {
+            String inputTeclado = ValidaLibrary.leer("Introduce la url: ");
+            ProcessBuilder pb = new ProcessBuilder(sRutaProceso, inputTeclado);
+            try {
+            // Lanzamos el proceso
+            Process pruses = pb.start();
 
-                    // Obtenemos estado de ejecucion
-                    int iRetorno = pruses.waitFor();
+            // Obtenemos estado de ejecucion
+            iRetorno = pruses.waitFor();
 
-                    System.out.println("La ejecucion del proceso " + sRutaProceso + " devuelve " + iRetorno);
-                }catch (IOException e) {
-                    System.out.println("Error IO: " + e.getMessage());
-                    System.exit(-1);
-                }catch (InterruptedException e) {
-                    System.out.println("Error IE: " + e.getMessage());
-                }
+            System.out.println("La ejecucion del proceso " + sRutaProceso + " devuelve " + iRetorno);
+            Thread.sleep(10000);
+            }catch (IOException e) {
+                System.out.println("Error IO: " + e.getMessage());
+                System.exit(-1);
+            }catch (InterruptedException e) {
+                System.out.println("Error IE: " + e.getMessage());
             }
-        }, 0, 10, TimeUnit.SECONDS);
+
+        }while (iRetorno == 0);
+
     }
 }
