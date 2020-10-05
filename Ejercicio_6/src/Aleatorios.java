d059f7da3d6b60cac0846a3333726f97103407b4
pabcavper <gelves.mula@gmail.com>
2020-10-05 14:31:20 +0200

    Terminada la clase Aleatorios

--- Ejercicio_6/src/Aleatorios.java
+++ Ejercicio_6/src/Aleatorios.java
@@ -1,2 +1,8 @@
+import java.util.Random;
+
 public class Aleatorios {
+    public static void generadorAleatorios() {
+        Random r = new Random();
+        int iNum = r.nextInt(10+1);
+    }
 }
