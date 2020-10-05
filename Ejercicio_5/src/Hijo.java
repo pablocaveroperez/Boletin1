15ec0ddc693ab8431336338975d2345545ad9d34
pabcavper <gelves.mula@gmail.com>
2020-10-05 14:27:05 +0200

    Terminado el ejercicio 5

--- Ejercicio_5/src/Hijo.java
+++ Ejercicio_5/src/Hijo.java
@@ -1,2 +1,8 @@
 public class Hijo {
+    public static int return2() throws InterruptedException {
+        Thread.sleep(3000);
+        int i = 2;
+        System.out.println("Proceso Hijo terminado");
+        return i;
+    }
 }
--- Ejercicio_5/src/Padre.java
+++ Ejercicio_5/src/Padre.java
@@ -1,2 +1,11 @@
+import validaciones.ValidaLibrary;
+
 public class Padre {
+    public static void main(String[] args) throws InterruptedException {
+        int iNumero = 1;
+        while (iNumero == 1) {
+            System.out.println(Hijo.return2());
+            iNumero = (int) ValidaLibrary.valida("Introduce un 1 para volver a ejecutarlo o un 0 para terminar: ", 0, 1, 1);
+        }
+    }
 }
