efd071ef15e36dc19d5f08775501977b0ad1ff81
pabcavper <gelves.mula@gmail.com>
2020-10-06 09:14:38 +0200

    Terminado el ejercicio 6

--- Ejercicio_6/src/Aleatorios.java
+++ Ejercicio_6/src/Aleatorios.java
@@ -1,9 +1,13 @@
 import java.util.Random;
 
 public class Aleatorios {
-    public static int generadorAleatorios() {
+    public static void main(String[] args) {
         Random r = new Random();
-        int iNum = r.nextInt(10+1);
-        return iNum;
+        int iNum = r.nextInt(10 + 1);
+        System.out.println("Numero generado aleatoriamente: " + iNum);
+        if (iNum > Integer.parseInt(args[0]) || iNum < Integer.parseInt(args[0]))
+            System.out.println("El numero aleatorio es diferente a el introducido");
+        else
+            System.out.println("El numero introducido y el aleatorio son iguales");
     }
 }
--- Ejercicio_6/src/Padre.java
+++ Ejercicio_6/src/Padre.java
@@ -1,9 +1,22 @@
-public class Padre {
-    public static void main(String[] args) {
-        int iSalir = 0;
-        int iNum;
-        while(iSalir == 0) {
+import validaciones.ValidaLibrary;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Padre {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int iSalir = 1;
+        int iNum;
+
+        List<String> jvmArgs = new ArrayList<>();
+        List<String> argsM = new ArrayList<>();
+
+
+        while(iSalir == 1) {
+            iNum = (int) ValidaLibrary.valida("Introduce un numero del 0 al 10: ", 0, 10, 1);
+            argsM.add(0, String.valueOf(iNum));
+            Process p = JavaProcess.exec(Aleatorios.class, jvmArgs, argsM);
+            iSalir = (int) ValidaLibrary.valida("Introduce un 1 para volver a ejecutarlo o un 0 para terminar: ", 0, 1, 1);
         }
     }
 
