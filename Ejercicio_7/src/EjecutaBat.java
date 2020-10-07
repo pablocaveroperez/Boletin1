0e657cd254fa40731de8203237e04eeb334f081c
pabcavper <gelves.mula@gmail.com>
2020-10-07 13:01:54 +0200

    Comenzado el Ejercicio 7

--- Ejercicio_7/src/EjecutaBat.java
+++ Ejercicio_7/src/EjecutaBat.java
@@ -1,2 +1,9 @@
+import java.io.IOException;
+
 public class EjecutaBat {
+    public static void main(String[] args) throws IOException {
+        Runtime.
+                getRuntime().
+                exec("cmd /c start \"\" ejecuta.bat");
+    }
 }
