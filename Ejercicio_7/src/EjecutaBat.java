bde797d2a95ca9fc0c3cae5e9fff6098c8c64a38
pabcavper <gelves.mula@gmail.com>
2020-10-08 12:13:56 +0200

    Creados los documentos de texto errorBat.txt ,  salidaBat.txt y mifichero.txt

--- Ejercicio_7/src/EjecutaBat.java
+++ Ejercicio_7/src/EjecutaBat.java
@@ -7,46 +7,8 @@ public class EjecutaBat {
     public static void main(String[] args) throws IOException {
         String OS = System.getProperty("os.name");
         if (OS.equals("Windows 10")){
-            ejecutarBat();
+            Runtime.getRuntime().exec("cmd.exe /c > ping ejecuta.bat >> salidaBat.txt 2>> errorBat.txt");
         }else
             System.out.println("Este programa solo se puede ejecutar en Windows 10");
-    }
-
-    private static void ejecutarBat() throws IOException {
-        final String FILE_SALIDA = "salidaBat.txt";
-        final String FILE_ERROR = "errorBat.txt";
-        Process process = Runtime.getRuntime().exec("cmd /c start \"\" ejecuta.bat");
-
-        BufferedReader salidaCMD = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-        BufferedReader salidaERROR = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-
-        // LEER LA SALIDA DE LA CONSOLA
-        String sCMD = null;
-        String salida = "";
-        while ((sCMD = salidaCMD.readLine()) != null) {
-            salida += sCMD;
-        }
-        escribirArchivo(FILE_SALIDA, salida);
-
-        // LEER LOS ERRORES DE LA CONSOLA
-        String sERROR = null;
-        salida = "";
-        while ((sERROR = salidaERROR.readLine()) != null) {
-            salida += sERROR;
-        }
-        escribirArchivo(FILE_ERROR, salida);
-    }
-
-    private static void escribirArchivo(String FILE_NAME, String sTexto) {
-        try {
-            FileWriter fchEscribir = new FileWriter(FILE_NAME);
-
-            fchEscribir.write(sTexto);
-
-            fchEscribir.close();
-        } catch (IOException e) {
-            System.err.println("Error IO: " + e.getMessage());
-        }
     }
 }
