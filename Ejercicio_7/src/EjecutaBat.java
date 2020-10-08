b178ff73757f95f61d4da8c14d4e15fb893a0e8a
pabcavper <gelves.mula@gmail.com>
2020-10-08 12:15:09 +0200

    Eliminados imports innecesarios

--- Ejercicio_7/src/EjecutaBat.java
+++ Ejercicio_7/src/EjecutaBat.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class EjecutaBat {
     public static void main(String[] args) throws IOException {
