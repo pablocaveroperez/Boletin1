ecf73e1970bbe93f2eb7714ae5b1f24a1e98691d
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-10-14 19:03:08 +0200

    CompararArchivos terminado

---
+++ Problema_2/src/CompararArchivos.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CompararArchivos {
+    public static void main(String[] args) {
+        Path path1 = Paths.get(args[0]);
+        Path path2 = Paths.get(args[1]);
+        String content1 = null;
+        String content2 = null;
+        try {
+            // Lee los ficheros de la ruta dada por el usuario
+            List<String> lines1 = Files.readAllLines(path1, StandardCharsets.UTF_8);
+            content1 = String.join(System.lineSeparator(), lines1);
+
+            List<String> lines2 = Files.readAllLines(path2, StandardCharsets.UTF_8);
+            content2 = String.join(System.lineSeparator(), lines2);
+        } catch (IOException e) {
+            System.err.println("Error IO: " + e.getMessage());;
+        }
+
+        if (content1.equals(content2))
+            System.out.println("Los do archisvo sons insguale.");
+    }
+}
--- Problema_2/src/Utilidades.java
+++ Problema_2/src/Utilidades.java
@@ -1,5 +1,22 @@
+import validaciones.ValidaLibrary;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Utilidades {
-    public static void main(String[] args) {
-        String valor1 =
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String valor1 = ValidaLibrary.leer("Introduce el primer parametro: ");
+        String valor2 = ValidaLibrary.leer("¿Quiere introducir un segundo parametro?(0-NO | 1-SI) ");
+        List<String> jvmArgs = new ArrayList<>();
+        List<String> argsM = new ArrayList<>();
+        Process process;
+
+        if (valor2.equals("1")) {
+            valor2 = ValidaLibrary.leer("Introduce el segundo parametro: ");
+            argsM.add(valor1);
+            argsM.add(valor2);
+            process = JavaProcess.exec(CompararArchivos.class, jvmArgs, argsM);
+        }
     }
 }
