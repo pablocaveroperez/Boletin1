c3ff84c35326b8b631421b950d01b1515862d849
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-10-14 16:28:43 +0200

    Creada la clase Traductor

--- Problema_1/src/Traductor.java
+++ Problema_1/src/Traductor.java
@@ -1,59 +1,18 @@
 import validaciones.ValidaLibrary;
 
 import java.io.IOException;
-import java.util.concurrent.*;
 
 public class Traductor {
-
-
-    public static void main(String[] args) throws InterruptedException {
-        String OS = System.getProperty("os.name");
-
-        if (OS.equals("Windows 10")) {
-            traductor();
-        } else if (OS.equals("Linux")) {
-            traductor();
-        } else
-            System.out.println("Este programa no se puede ejecutar en este equipo");
-    }
-
-    private static void traductor() {
-        int iTiempoMax = (int) ValidaLibrary.valida("Introduce el tiempo maximo de ejecucion del proceso: ", 1,100,1);
-        TraducirThread proceso = new TraducirThread();
-
-        proceso.start();
-
-        try {
-            Thread.sleep(iTiempoMax);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        proceso.stopRunning();
-    }
-}
-
-class TraducirThread extends Thread {
     private static String sIdioma1;
     private static String sIdioma2;
     private static String sFrase;
 
-    private volatile boolean flag = true;
-
-    public void stopRunning() {
-        flag = false;
+    public static void main(String[] args) {
+        traductor();
     }
 
-    @Override
-    public void run() {
-        while(flag) {
-            traductor();
-        }
-        System.out.println("Tiempo terminado");
-    }
-
-     public void traductor() {
+    public static void traductor() {
         String sRuta = ValidaLibrary.leer("Introduce la ruta del exe del navegador: ");
-
         sIdioma1 = ValidaLibrary.leer("Introduce el idioma del que vas a traducir.(Por ejemplo: es = Spanish, en = english)");
         sIdioma2 = ValidaLibrary.leer("Introduce el idioma al que quieres traducir.");
         sFrase = ValidaLibrary.leer("Introduce la frase a traducir: ");
@@ -75,4 +34,3 @@ class TraducirThread extends Thread {
         }
     }
 }
-
