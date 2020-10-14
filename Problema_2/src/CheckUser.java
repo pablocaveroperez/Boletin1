4d5143ba36706975fb420245e314b7755ee71ccf
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-10-14 19:42:50 +0200

    Terminando la clase CheckUser y algún arreglo en la clase Utilidades

--- Problema_2/src/CheckUser.java
+++ Problema_2/src/CheckUser.java
@@ -1,7 +1,17 @@
+import java.io.IOException;
+
 public class CheckUser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String user = args[0];
+        String OS = System.getProperty("os.name");
+        if (OS.equals("Windows 10")) {
+            Runtime.getRuntime().exec("cmd.exe /c > ping net user " + user + " 2>> errorBat.txt");
 
-
+        }else if (OS.equals("Linux")) {
+            String[] terminalLinux = new String[] {"/bin/bash", "-c", "cat /etc/passwd", "with", "args"};
+            Process proc = new ProcessBuilder(terminalLinux).start();
+        }else {
+            System.err.println("Sistema operativo no detectado. Campeon");
+        }
     }
 }
--- Problema_2/src/Utilidades.java
+++ Problema_2/src/Utilidades.java
@@ -7,16 +7,20 @@ import java.util.List;
 public class Utilidades {
     public static void main(String[] args) throws IOException, InterruptedException {
         String valor1 = ValidaLibrary.leer("Introduce el primer parametro: ");
-        String valor2 = ValidaLibrary.leer("¿Quiere introducir un segundo parametro?(0-NO | 1-SI) ");
+        String valor2 = ValidaLibrary.leer("¿Quiere introducir un segundo parametro?(1-SI | NO-Cualquier otro caracter) ");
         List<String> jvmArgs = new ArrayList<>();
         List<String> argsM = new ArrayList<>();
         Process process;
+        argsM.add(valor1);
 
         if (valor2.equals("1")) {
             valor2 = ValidaLibrary.leer("Introduce el segundo parametro: ");
-            argsM.add(valor1);
+
             argsM.add(valor2);
             process = JavaProcess.exec(CompararArchivos.class, jvmArgs, argsM);
+        }else {
+            process = JavaProcess.exec(CheckUser.class, jvmArgs, argsM);
         }
+
     }
 }
