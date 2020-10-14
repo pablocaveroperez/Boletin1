277b2152383a2c2d70be1dc96071cb6adff44c98
pabcavper <gelves.mula@gmail.com>
2020-10-14 12:51:57 +0200

    Intentando poner el limitador de tiempo del proceso

--- Problema_1/src/Traductor.java
+++ Problema_1/src/Traductor.java
@@ -4,71 +4,75 @@ import java.io.IOException;
 import java.util.concurrent.*;
 
 public class Traductor {
-    private static int iTiempoMax = (int) ValidaLibrary.valida("Introduce el tiempo maximo de ejecucion del proceso: ", 1,10,1);
-    private static String sIdioma1 = ValidaLibrary.leer("Introduce el idioma del que vas a traducir.(Por ejemplo: es = Spanish, en = english)");
-    private static String sIdioma2 = ValidaLibrary.leer("Introduce el idioma al que quieres traducir.");
-    private static String sFrase = ValidaLibrary.leer("Introduce la frase a traducir: ");
+
 
     public static void main(String[] args) throws InterruptedException {
         String OS = System.getProperty("os.name");
-        String sRutaAcceso = args[0];
-
-
 
         if (OS.equals("Windows 10")) {
-            traductor(sRutaAcceso);
-        }else if (OS.equals("Linux")) {
-            traductor(sRutaAcceso);            //        <-- your job
-        }else
+            traductor();
+        } else if (OS.equals("Linux")) {
+            traductor();
+        } else
             System.out.println("Este programa no se puede ejecutar en este equipo");
     }
 
-    private static void traductor(String sRuta) {
-        byte bSalidaBucle = 0;
-        do {
-            ExecutorService executor = Executors.newFixedThreadPool(4);
+    private static void traductor() {
+        int iTiempoMax = (int) ValidaLibrary.valida("Introduce el tiempo maximo de ejecucion del proceso: ", 1,100,1);
+        TraducirThread proceso = new TraducirThread();
 
-            Future<?> future = executor.submit(new Runnable() {
-                @Override
-                public void run() {
-                    sIdioma1 = ValidaLibrary.leer("Introduce el idioma del que vas a traducir.(Por ejemplo: es = Spanish, en = english)");
-                    sIdioma2 = ValidaLibrary.leer("Introduce el idioma al que quieres traducir.");
-                    sFrase = ValidaLibrary.leer("Introduce la frase a traducir: ");
-                    if (sIdioma1.equals(""))
-                        sIdioma1 = "es";
-                    if (sIdioma2.equals(""))
-                        sIdioma2 = "en";
-                    if (sFrase.equals(""))
-                        sFrase = "Pon una frase para traducirla, campeon";
-                    String EN_TO_SP = "http://translate.google.es/?hl="+sIdioma1+"#auto/"+ sIdioma2 +"/"+sFrase;
+        proceso.start();
 
-                    ProcessBuilder pb = new ProcessBuilder(sRuta, EN_TO_SP);            //        <-- your job
-                    try {
-                        // Lanzamos el proceso
-                        pb.start();
-                    }catch (IOException e) {
-                        System.out.println("Error IO: " + e.getMessage());
-                        System.exit(-1);
-                    }
-
-                }
-
-            });
-
-            try {
-                future.get(iTiempoMax, TimeUnit.SECONDS);  //     <-- wait 8 seconds to finish
-            } catch (InterruptedException e) {    //     <-- possible error cases
-                System.out.println("job was interrupted");
-            } catch (ExecutionException e) {
-                System.out.println("caught exception: " + e.getCause());
-            } catch (TimeoutException e) {
-                future.cancel(true);              //     <-- interrupt the job
-                System.out.println("timeout");
-            }
-            bSalidaBucle = (byte) ValidaLibrary.valida("Introduce un 0 para terminar o un 1 para volver a ejecutarlo", 0, 1, 3);
-            iTiempoMax = (int) ValidaLibrary.valida("Introduce el tiempo maximo de ejecucion del proceso: ", 1,10,1);
-
-        }while (bSalidaBucle == 1);
-
+        try {
+            Thread.sleep(iTiempoMax);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        proceso.stopRunning();
     }
 }
+
+class TraducirThread extends Thread {
+    private static String sIdioma1;
+    private static String sIdioma2;
+    private static String sFrase;
+
+    private volatile boolean flag = true;
+
+    public void stopRunning() {
+        flag = false;
+    }
+
+    @Override
+    public void run() {
+        while(flag) {
+            traductor();
+        }
+        System.out.println("Tiempo terminado");
+    }
+
+     public void traductor() {
+        String sRuta = ValidaLibrary.leer("Introduce la ruta del exe del navegador: ");
+
+        sIdioma1 = ValidaLibrary.leer("Introduce el idioma del que vas a traducir.(Por ejemplo: es = Spanish, en = english)");
+        sIdioma2 = ValidaLibrary.leer("Introduce el idioma al que quieres traducir.");
+        sFrase = ValidaLibrary.leer("Introduce la frase a traducir: ");
+        if (sIdioma1.equals(""))
+            sIdioma1 = "es";
+        if (sIdioma2.equals(""))
+            sIdioma2 = "en";
+        if (sFrase.equals(""))
+            sFrase = "Pon una frase para traducirla, campeon";
+        String EN_TO_SP = "http://translate.google.es/?hl="+sIdioma1+"#auto/"+ sIdioma2 +"/"+sFrase;
+
+        ProcessBuilder pb = new ProcessBuilder(sRuta, EN_TO_SP);            //        <-- your job
+        try {
+            // Lanzamos el proceso
+            pb.start();
+        }catch (IOException e) {
+            System.out.println("Error IO: " + e.getMessage());
+            System.exit(-1);
+        }
+    }
+}
+
