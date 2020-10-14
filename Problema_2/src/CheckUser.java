36e9fd7c6b8d1d065139d870b74322987ee33b34
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-10-14 22:00:17 +0200

    Terminanda la parte de windows de CheckUser

--- Problema_2/src/CheckUser.java
+++ Problema_2/src/CheckUser.java
@@ -1,12 +1,22 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class CheckUser {
     public static void main(String[] args) throws IOException {
         String user = args[0];
         String OS = System.getProperty("os.name");
         if (OS.equals("Windows 10")) {
-            Runtime.getRuntime().exec("cmd.exe /c > salidaCMD.txt net user " + user + " 2>> ERRORES.DAT");
-
+            Process process = Runtime.getRuntime().exec("cmd.exe /c net user " + user + " 2>> ERRORES.DAT");
+            BufferedReader salidaCMD = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String s = salidaCMD.readLine();
+            if (s == null) {
+                System.err.println("Usuario no encontrado. Error añadido al archivo ERRORES.DAT");
+            }
+            while (s != null) {
+                System.out.println(s);
+                s = salidaCMD.readLine();
+            }
         }else if (OS.equals("Linux")) {
             Process process = Runtime.getRuntime().exec("grep -c " + user);
         }else {
