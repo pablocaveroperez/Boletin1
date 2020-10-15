c2ec852bcb732ac9d93e0101e06cea220cf23b9e
pabcavper <gelves.mula@gmail.com>
2020-10-15 11:55:09 +0200

    Check User para Linux

--- Problema_2/src/CheckUser.java
+++ Problema_2/src/CheckUser.java
@@ -18,7 +18,7 @@ public class CheckUser {
                 s = salidaCMD.readLine();
             }
         }else if (OS.equals("Linux")) {
-            Process process = Runtime.getRuntime().exec("cut -d : -f 1 /etc/passwd");
+            Process process = Runtime.getRuntime().exec("cut -d : -f 1 /etc/passwd 2>>ERRORES.DAT");
             BufferedReader salidaCMD = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String s = salidaCMD.readLine();
             while (s != null && !s.equals(user)) {
