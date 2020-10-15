2724e5d1b0ee5201f0caa6cd2391e576abfc99a8
Pablo Cavero Perez <gelves.mula@gmail.com>
2020-10-15 20:40:27 +0200

    Optimizado el codigo de la clase EjecutaBat

--- Ejercicio_7/src/EjecutaBat.java
+++ Ejercicio_7/src/EjecutaBat.java
@@ -5,10 +5,10 @@ import java.util.List;
 public class EjecutaBat {
     public static void main(String[] args) throws IOException, InterruptedException {
         String OS = System.getProperty("os.name");
-        List<String> jvmArgs = new ArrayList<>();
-        List<String> argsM = new ArrayList<>();
 
         if (OS.equals("Windows 10")){
+            List<String> jvmArgs = new ArrayList<>();
+            List<String> argsM = new ArrayList<>();
             JavaProcess.exec(CmdExecuter.class, jvmArgs, argsM);
         }else
             System.out.println("Este programa solo se puede ejecutar en Windows 10");
